568a0ce10495a2fa3f518631d587c0793c541acd
malive939-eng <malive939@gmail.com>
2025-08-18 21:57:30 +0530

    Update main.js

--- src/commands/main.js
+++ src/commands/main.js
@@ -1,2 +1,35 @@
+const { from } = require("form-data")
+
 conts os = require("os") 
 conts (runtime) = require ("../utils/functions"
+module.exports = [
+    {
+        name:"alive",
+        desdedescription:"alive command",
+        react:"👨🏻‍💻",
+        owneronly: false,
+        grouponly: false,
+        admingonly: false,
+        botabmin: false,
+        esync execute(conn,mek,args,context){
+            conts {from,pushname,reply,quoted} = context;
+            try{
+                let desc = `
+Hey ${pushname} I am alive
+uptime:${runtime(process. uptime())}
+`
+await conn.sendMessage(from,
+    {
+        image:{url:"https://raw.githubusercontent.com/malive939-eng/Sofia-help/refs/heads/main/pixlr-image-generator-689ea750aed9a8e28945f0f5.jpg"},
+        caption: desc
+    },{quoted:mek
+
+    }
+)
+
+            }catch(e){
+                console.log(e)
+            }
+        }
+    }
+]
